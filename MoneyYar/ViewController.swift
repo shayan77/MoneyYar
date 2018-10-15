e7875807afd05e2bc85e9777a900aff3017494a8
Shayan Mehranpoor <shayanmehranpoor@yahoo.com>
2018-10-15 10:43:32 +0330

    Initial Commit

---
+++ MoneyYar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyYar
+//
+//  Created by Shayan Mehranpoor on 10/15/18.
+//  Copyright © 2018 Shayan Mehranpoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
