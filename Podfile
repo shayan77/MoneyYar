ef886cd78a671e5c394f0c46110993844c84d640
Shayan Mehranpoor <shayanmehranpoor@yahoo.com>
2018-10-16 12:13:49 +0330

    add pod

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '11.0'
+
+target 'MoneyYar' do
+  use_frameworks!
+
+  pod 'M13Checkbox'
+  pod 'RxSwift'
+  pod 'RxCocoa'
+
+end
---
+++ Pods/Target Support Files/Pods-MoneyYar/Pods-MoneyYar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MoneyYar : NSObject
+@end
+@implementation PodsDummy_Pods_MoneyYar
+@end
